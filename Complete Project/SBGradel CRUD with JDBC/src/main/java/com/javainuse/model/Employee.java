bd6e345ebbffc1fed93fa4c1893b1014e7f66843
Aditya Singh <110605212+AdityaSinghGlobalLogic@users.noreply.github.com>
2022-09-05 10:24:55 +0530

    Add files via upload

---
+++ Complete Project/SBGradel CRUD with JDBC/src/main/java/com/javainuse/model/Employee.java
@@ -0,0 +1,57 @@
+package com.javainuse.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employeesgradel")
+public class Employee {
+	@Id
+	private long empId;
+	@Column(name = "Name")
+	private String name;
+	@Column(name = "Designation")
+	private String designation;
+	@Column(name = "Salary")
+	private double salary;
+
+	public Employee() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+
+}
---
+++ Que1 CRUD with Gradel/SbGradelApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGradelApplication.class, args);
+	}
+
+}
